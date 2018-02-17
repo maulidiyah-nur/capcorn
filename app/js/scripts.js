d643121a990faca487341bd16648cdaa67dbd5a3
Nur Maulidiyah <nur.maulidiyah20@gmail.com>
2018-02-17 16:40:51 +0700

    simple content loader

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -3,15 +3,39 @@ $(document).ready(function(){
         $('.menu-res, .dropdown-res-menu').toggleClass('open');
     });
 
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({
-        items:1,
-        margin:10,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout:10000,
-        autoplayHoverPause:true,
-        dot: true
+    /*
+     * content loader
+     * for FE dev purpose only 
+    */
+    $('[data-load]').click(function(){
+        var htmlSource = $(this).data('load');
+        loadContent(htmlSource);
     });
+
+    function loadContent(source){
+        $(".content-wrapper").load("html/"+source, function( response, status, xhr ) {
+            if ( status == "error" ) {
+              var msg = "Sorry but there was an error: ";
+              alert( msg + xhr.status + " " + xhr.statusText );
+            }
+            else{
+                var owl = $('.owl-carousel');
+                owl.owlCarousel({
+                    items:1,
+                    margin:10,
+                    loop: true,
+                    autoplay: true,
+                    autoplayTimeout:10000,
+                    autoplayHoverPause:true,
+                    dot: true
+                });
+            }
+
+            $('.menu-item').removeClass("active");
+            $("[data-load='"+source+"']").addClass("active");
+        });
+    }
+
+    loadContent("home.html");
 
 })
